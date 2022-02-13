2f643af6e69b65c355509bbfe41868783d07e862
Ryan P. Daly <mathemaphysics@gmail.com>
2022-02-13 19:11:59 +0000

    Updated unit tests to test FWorker similarly to IWorker

--- tests/agent_t.cpp
+++ tests/agent_t.cpp
@@ -34,20 +34,20 @@ protected:
 class FWorkerTest : public ::testing::Test
 {
 public:
-  static int ProcessMessage(void* _msg, std::uint32_t)
+  static int ProcessMessage(const void* _msg, std::uint32_t)
   {
     auto msg = Messages::GetMessage(_msg);
     auto id = msg->id();
     auto height = msg->height();
     auto width = msg->width();
     auto pixels = msg->pixels()->Data();
-
+    
     return 0;
   }
 protected:
   void SetUp() override
   {
-    worker = new FWorker(0, "WorkerTest", );
+    worker = new FWorker(0, "FWorkerTest", ProcessMessage);
     worker->Run(3);
   }
 
@@ -76,6 +76,62 @@ protected:
 };
 
 TEST_F(WorkerTest, CreateWorker)
+{
+  flatbuffers::FlatBufferBuilder builder(AGENT_FB_BUFFER_SIZE);
+  auto pixels = builder.CreateVector(
+    std::vector<std::int8_t>{
+      { 0, 0, 1,
+        2, 2, 1,
+        4, 0, 1 }
+    }
+  );
+
+  // Build message 1
+  auto message1 = Messages::CreateMessage(builder, 0, 3, 3, pixels);
+  builder.Finish(message1);
+  auto buffer1 = builder.GetBufferPointer();
+  auto size = builder.GetSize();
+
+  // Build message 2
+  auto message2 = Messages::CreateMessage(builder, 1, 3, 3, pixels);
+  builder.Finish(message2);
+  auto buffer2 = builder.GetBufferPointer();
+
+  // Build message 3
+  auto message3 = Messages::CreateMessage(builder, 2, 3, 3, pixels);
+  builder.Finish(message3);
+  auto buffer3 = builder.GetBufferPointer();
+
+  // Add the item for the worker to process
+  for (int i = 0; i < 4; i++)
+  {
+    worker->AddMessage(buffer1, size);
+    worker->AddMessage(buffer2, size);
+    worker->AddMessage(buffer3, size);
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
+
+  // Exit the thread
+  worker->Stop();
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  worker->Run();
+
+  // Add the item for the worker to process
+  for (int i = 0; i < 4; i++)
+  {
+    worker->AddMessage(buffer1, size);
+    worker->AddMessage(buffer2, size);
+    worker->AddMessage(buffer3, size);
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
+
+  // Quit all threads
+  worker->SetQuit();
+
+  EXPECT_EQ(1, 1);
+}
+
+TEST_F(FWorkerTest, CreateWorker)
 {
   flatbuffers::FlatBufferBuilder builder(AGENT_FB_BUFFER_SIZE);
   auto pixels = builder.CreateVector(
