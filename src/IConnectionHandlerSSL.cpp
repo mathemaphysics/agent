59ec150900b6c18db58a553626a5144d7eea901c
Ryan P. Daly <mathemaphysics@gmail.com>
2022-06-19 14:51:58 +0000

    Created IConnectionHandlerSSL for use in IAMQPWorker.

---
+++ src/IConnectionHandlerSSL.cpp
@@ -0,0 +1,259 @@
+#include "agent/agent.hpp"
+#include "IConnectionHandlerSSL.hpp"
+#include "IWorker.hpp"
+
+#include <vector>
+#include <cstdint>
+#include <string>
+#include <sstream>
+#include <exception>
+
+#include <amqpcpp.h>
+#include <spdlog/spdlog.h>
+#include <Poco/Net/SecureStreamSocket.h>
+#include <Poco/Crypto/X509Certificate.h>
+#include <Poco/Net/Context.h>
+
+agent::IConnectionHandlerSSL::IConnectionHandlerSSL(unsigned int _id, const Poco::Net::Context& __context)
+    : _client("IConnectionHandler"), // Default client name
+      _connected(false),
+      _connection(nullptr),
+      _inpbuffer(AGENT_CONN_BUFFER_SIZE),
+      _tmpbuffer(AGENT_CONN_TEMP_BUFFER_SIZE),
+      _outbuffer(AGENT_CONN_BUFFER_SIZE),
+      _address(Poco::Net::SocketAddress("localhost", 5672)),
+      _context(__context),
+      _logger(nullptr), // Default no logger
+      IWorker(_id)
+{
+  // Check if logger called GetName() exists, else create it
+  _logger = spdlog::get(_client);
+  if (_logger == nullptr)
+    _logger = spdlog::stdout_color_mt(_client);
+
+  // Just announce the creation of the client; can turn this off via log level
+  _logger->info("Client {} created", _client);
+
+  // Set up the AMQP::Connection here and then Run()
+  // Set up context here
+  //_context.addCertificateAuthority(Poco::Crypto::X509Certificate(std::string("certauth.crt")));
+  _socket.connect(_address);
+  _socket.setKeepAlive(true);
+}
+
+agent::IConnectionHandlerSSL::IConnectionHandlerSSL(
+    unsigned int _id,
+    const std::string& _host,
+    std::uint16_t _port,
+    const std::string& _name,
+    const Poco::Net::Context& __context,
+    const std::string& __product,
+    const std::string& __version,
+    const std::string& __copyright,
+    const std::string& __information
+  )
+    : _client(_name),
+      _context(__context),
+      _product(__product),
+      _version(__version),
+      _copyright(__copyright),
+      _information(__information),
+      _connected(false),
+      _connection(nullptr),
+      _inpbuffer(AGENT_CONN_BUFFER_SIZE),
+      _tmpbuffer(AGENT_CONN_TEMP_BUFFER_SIZE),
+      _outbuffer(AGENT_CONN_BUFFER_SIZE),
+      _address(Poco::Net::SocketAddress(_host, _port)),
+      IWorker(_id, _name)
+{
+  // Check if logger called GetName() exists, else create it
+  _logger = spdlog::get(_client);
+  if (_logger == nullptr)
+    _logger = spdlog::stdout_color_mt(_client);
+  
+  // Just announce the creation of the client; can turn this off via log level
+  _logger->info("Client {} created", _client);
+
+  // Set up the AMQP::Connection here and then Run()
+  _socket.connect(_address);
+  _socket.setKeepAlive(true);
+}
+
+void agent::IConnectionHandlerSSL::onProperties(AMQP::Connection *__connection, const AMQP::Table &_server, AMQP::Table &__client)
+{
+  if (_connection == nullptr)
+    _connection = __connection;
+
+  // Make sure we know who you are
+  __client["connection_name"] = _client;
+  __client["product"] = _product;
+  __client["version"] = _version;
+  __client["copyright"] = _copyright;
+  __client["information"] = _information;
+
+  // Set the platform
+  #if defined(__linux__)
+  __client["platform"] = "Linux";
+  #elif defined(__APPLE__)
+  __client["platform"] = "Mac OS X";
+  #elif defined(__sun)
+  __client["platform"] = "Solaris";
+  #elif defined(__FreeBSD__)
+  __client["platform"] = "FreeBSD";
+  #elif defined(__OpenBSD__)
+  __client["platform"] = "OpenBSD";
+  #elif defined(__NetBSD__)
+  __client["platform"] = "NetBSD";
+  #elif defined(__hpux)
+  __client["platform"] = "HP-UX";
+  #elif defined(__osf__)
+  __client["platform"] = "Tru64 UNIX";
+  #elif defined(__sgi)
+  __client["platform"] = "Irix";
+  #elif defined(_AIX)
+  __client["platform"] = "AIX";
+  #elif defined(_WIN32)
+  __client["platform"] = "Windows";
+  #endif
+
+  // Print details of the client and server
+  auto _clientss = std::ostringstream();
+  auto _serverss = std::ostringstream();
+
+  _clientss << _client;
+  _serverss << _server;
+
+  _logger->info("[onProperties] Client: {}, Server: {}", _clientss.str(), _serverss.str());
+}
+
+uint16_t agent::IConnectionHandlerSSL::onNegotiate(AMQP::Connection *__connection, uint16_t _interval)
+{
+  if (_connection == nullptr)
+    _connection = __connection;
+
+  // Print details of the heartbeat negotiation
+  _logger->info("[onNegotiate] Accepting interval of length {}", _interval);
+
+  // Just accept the interval
+  return _interval;
+}
+
+void agent::IConnectionHandlerSSL::onData(AMQP::Connection *__connection, const char *_data, size_t _size)
+{
+  if (_connection == nullptr)
+    _connection = __connection;
+
+  // Send any outgoing data that has shown up in the buffer
+  _socket.sendBytes(_data, _size);
+
+  // TODO: Maybe make this debug level?
+  _logger->debug("[onData] Sent {} bytes", _size);
+}
+
+void agent::IConnectionHandlerSSL::onHeartbeat(AMQP::Connection *__connection)
+{
+  if (_connection == nullptr)
+    _connection = __connection;
+
+  // Announce that we received a heartbeat from the AMQP server
+  _logger->debug("[onHeartbeat] Received a heartbeat from server");
+
+  // Send a return heartbeat; this isn't ideal because it depends on receiving a
+  // heartbeat first, which might not happen. Set up an independent thread that
+  // takes care of this
+  _connection->heartbeat();
+}
+
+void agent::IConnectionHandlerSSL::onError(AMQP::Connection *__connection, const char *_message)
+{
+  if (_connection == nullptr)
+    _connection = __connection;
+
+  // Announce that we received a heartbeat from the AMQP server
+  _logger->error("[onError] Error: {}", _message);
+}
+
+void agent::IConnectionHandlerSSL::onReady(AMQP::Connection *__connection)
+{
+  if (_connection == nullptr)
+    _connection = __connection;
+
+  // Notify the log that the connection is read
+  _logger->info("[onReady] Connection is ready");
+}
+
+void agent::IConnectionHandlerSSL::onClosed(AMQP::Connection *__connection)
+{
+  if (_connection == nullptr)
+    _connection = __connection;
+
+  // Announce and close the connection
+  _logger->info("[onClosed] Connection closed");
+
+  // Exit the loop
+  quit();
+}
+
+// This function should not exist at all; why can't I get rid of it?
+int agent::IConnectionHandlerSSL::ProcessMessage(const void*_msg, std::uint32_t _size, void*_result, std::uint32_t*_rsize)
+{
+  return 0;
+}
+
+void agent::IConnectionHandlerSSL::operator()()
+{
+  // This is the main worker loop for AMQP transactions
+  while (GetState() != WORKER_QUIT)
+  {
+    // See if there's any data available on the incoming socket
+    const size_t savail = _socket.available();
+    if (savail > 0)
+    {
+      // You might have to resize for larger incoming chunk
+      if (savail > _tmpbuffer.size())
+        _tmpbuffer.resize(savail, 0);
+      
+      // Make sure all bytes read were processed
+      const int rbytes = _socket.receiveBytes(_tmpbuffer.data(), savail);
+      const int wbytes = _inpbuffer.Write(_tmpbuffer.data(), rbytes);
+
+      if (wbytes != rbytes)
+        _logger->debug("Could not write full contents to input buffer");
+    }
+    else if (savail < 0)
+    {
+      _logger->error("Socket error: Available bytes on socket < 0");
+    }
+
+    const size_t iavail = _inpbuffer.Available();
+    if (iavail > 0)
+    {
+      const size_t parsed = _connection->parse(_inpbuffer.Data(), iavail);
+
+      if (parsed == iavail)
+        _inpbuffer.Drain();
+      else if (parsed > 0)
+        _inpbuffer.Shift(parsed);
+    }
+    _sendDataFromBuffer();
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+
+  if (GetState() == WORKER_QUIT && _outbuffer.Available())
+    _sendDataFromBuffer();
+}
+
+void agent::IConnectionHandlerSSL::quit()
+{
+  SetQuit();
+}
+
+void agent::IConnectionHandlerSSL::_sendDataFromBuffer()
+{
+  size_t avail = _outbuffer.Available();
+  if (avail > 0)
+  {
+    int sent = _socket.sendBytes(_outbuffer.Data(), avail);
+    _logger->info("Sent [{:6d} / {:6d}] bytes from buffer", sent, avail);
+  }
+}
---
+++ src/IConnectionHandlerSSL.hpp
@@ -0,0 +1,142 @@
+#pragma once
+
+#include "Buffer.hpp"
+#include "IWorker.hpp"
+
+#include <amqpcpp.h>
+#include <Poco/Net/SecureStreamSocket.h>
+#include <Poco/Crypto/X509Certificate.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+#include <vector>
+#include <cstdint>
+
+namespace agent
+{
+	class IConnectionHandlerSSL : public AMQP::ConnectionHandler, public IWorker
+	{
+	public:
+		IConnectionHandlerSSL(unsigned int __id, const Poco::Net::Context& __context);
+
+		/**
+		 * @brief Construct a new connection potentially with SSL
+		 * 
+		 * @param __id 
+		 * @param _host 
+		 * @param _port 
+		 * @param _name 
+		 * @param __context 
+		 * @param __product 
+		 * @param __version 
+		 * @param __copyright 
+		 * @param __information 
+		 */
+		IConnectionHandlerSSL(unsigned int __id, const std::string& _host, std::uint16_t _port, const std::string& _name, const Poco::Net::Context& __context, const std::string& __product = "", const std::string& __version = "", const std::string& __copyright = "", const std::string& __information = "");
+
+		/**
+		 * @brief Destroy the Connection Handler object
+		 * 
+		 * Not default because we need to shut off the \c AMQP::Connection close
+		 * the \c StreamSocket and delete the pointer to \c _connection
+		 */
+		~IConnectionHandlerSSL() = default;
+
+		/**
+		 * @brief Callback for the properties action from AMQP server
+		 * 
+		 * @param _connection The connection object, \c AMQP::Connection 
+		 * @param _server Server information table
+		 * @param _client Client information table
+		 */
+		void onProperties(AMQP::Connection* _connection, const AMQP::Table& _server, AMQP::Table& _client) override;
+
+		/**
+		 * @brief Callback for negotiating the heartbeat interval
+		 * 
+		 * @param _connection The connection object, \c AMQP::Connection
+		 * @param _interval Requested heartbeat interval
+		 * @return uint16_t Negotiated heartbeat interval
+		 */
+		uint16_t onNegotiate(AMQP::Connection* _connection, uint16_t _interval) override;
+
+		/**
+		 * @brief Callback which acts to send data when present
+		 * 
+		 * @param _connection The connection object, \c AMQP::Connection
+		 * @param _data Pointer to the bytes to send
+		 * @param _size Number of bytes to send
+		 */
+		void onData(AMQP::Connection* _connection, const char* _data, size_t _size) override;
+
+		/**
+		 * @brief Callback which handles case when heartbeat is received
+		 * 
+		 * @param _connection The connection object, \c AMQP::Connection
+		 */
+		void onHeartbeat(AMQP::Connection* _connection) override;
+
+		/**
+		 * @brief Callback for error case
+		 * 
+		 * @param _connection The connection object, \c AMQP::Connection
+		 * @param _message Error message
+		 */
+		void onError(AMQP::Connection* _connection, const char* _message) override;
+
+		/**
+		 * @brief Callback which fires when connection is ready
+		 * 
+		 * @param _connection The connection object, \c AMQP::Connection
+		 */
+		void onReady(AMQP::Connection* _connection) override;
+
+		/**
+		 * @brief Callback for closure of the connection
+		 * 
+		 * @param _connection The connection object, \c AMQP::Connection
+		 */
+		void onClosed(AMQP::Connection* _connection) override;
+
+		/**
+		 * @brief Overridden function which won't be called
+		 * 
+		 * @param _msg N/A
+		 * @param _size N/A
+		 * @return int N/A
+		 */
+		int ProcessMessage(const void* _msg, std::uint32_t _size, void* _result = nullptr, std::uint32_t* _rsize = nullptr);
+
+		/**
+		 * @brief Function which runs the loop
+		 * 
+		 */
+		void operator()() override;
+
+		/**
+		 * @brief Sets the quit state to kill the loop
+		 * 
+		 */
+		void quit();
+
+	protected:
+		std::shared_ptr<spdlog::logger> _logger;
+
+	private:
+		void _connectSocket(Poco::Net::SocketAddress _address);
+		std::string _client;
+		std::string _product;
+		std::string _version;
+		std::string _copyright;
+		std::string _information;
+		bool _connected;
+		Poco::Net::SecureStreamSocket _socket;
+		const Poco::Net::Context& _context;
+		const Poco::Net::SocketAddress _address;
+		AMQP::Connection* _connection;
+		Buffer _inpbuffer;
+		Buffer _outbuffer;
+		std::vector<char> _tmpbuffer;
+		void _sendDataFromBuffer();
+	};
+}
